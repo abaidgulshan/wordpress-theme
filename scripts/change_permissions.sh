a3d8f798b4637d708a55ed3a1e1bf679f65d2e89
abaid778 <abaid.gulshan@gmail.com>
2016-07-05 12:44:57 +0000

    changes files

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /home/ec2-user/independent-publisher && mv independent-publisher /var/www/html/wp-content/themes
+cd /root/wordpress 
+cp -r *  /var/www/html
+cd /home/ec2-user/independent-publisher/independent-publisher
+cp -r * /root/wordpress/wp-content/themes/independent-publisher
 chown -R apache:apache /var/www/html
 chmod -R 744 /var/www/html
 
